6d544350587e59e8fb5b1d6298bb371e03a57494
Felipe Dornelles <lipe.dornelles@hotmail.com>
2023-07-12 17:11:06 -0300

    feat: pets repository

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,38 @@
+import { randomUUID } from "crypto";
+import { Pet, Prisma } from "@prisma/client";
+import { PetsRepository } from "../pets-repository";
+import { Decimal } from "@prisma/client/runtime";
+
+export class InMemoryPetsRepository implements PetsRepository {
+  public items: Pet[] = [];
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = {
+      id: data.id ?? randomUUID(),
+      species: data.species,
+      age: new Decimal(data.age.toString()),
+      city: data.city,
+      org_id: data.org_id,
+      adopted_at: data.adopted_at ? new Date(data.adopted_at) : null,
+      created_at: new Date(),
+    };
+
+    this.items.push(pet);
+
+    return pet;
+  }
+
+  async findById(id: string) {
+    const pet = this.items.find((pet) => pet.id === id);
+
+    if (!pet) return null;
+
+    return pet;
+  }
+
+  async findManyByCity(city: string) {
+    const pets = this.items.filter((pets) => pets.city === city);
+
+    return pets;
+  }
+}
---
+++ src/repositories/pets-repository.ts
@@ -0,0 +1,7 @@
+import { Pet, Prisma } from "@prisma/client";
+
+export interface PetsRepository {
+  create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>;
+  findById(id: string): Promise<Pet | null>;
+  findManyByCity(city: string): Promise<Pet[]>;
+}
