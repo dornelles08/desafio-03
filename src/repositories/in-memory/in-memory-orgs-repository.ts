347e04a638ee30d50d910cb44e4a4652aed473da
Felipe Dornelles <lipe.dornelles@hotmail.com>
2023-07-12 15:44:26 -0300

    feat: create repository for org

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,39 @@
+import { Org, Prisma } from "@prisma/client";
+import { OrgsRepository } from "../orgs-repository";
+import { randomUUID } from "crypto";
+
+export class InMemoryOrgsRepository implements OrgsRepository {
+  public items: Org[] = [];
+
+  async create(data: Prisma.OrgCreateInput) {
+    const org = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      address: data.address,
+      whatsapp_number: data.whatsapp_number,
+      created_at: new Date(),
+    };
+
+    this.items.push(org);
+
+    return org;
+  }
+
+  async findById(id: string) {
+    const org = this.items.find((org) => org.id === id);
+
+    if (!org) return null;
+
+    return org;
+  }
+
+  async findByEmail(email: string) {
+    const org = this.items.find((org) => org.email === email);
+
+    if (!org) return null;
+
+    return org;
+  }
+}
